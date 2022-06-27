3dcf736e4791db7c895896e5ffe02d7d9e16cc87
r-scheele <abdulrahmanolamilekan88@gmail.com>
2022-06-27 14:28:08 +0100

    [Feature] Added a route to retrieve data from a posgres database

--- app/server/api.py
+++ app/server/api.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from app.server.auth.authenticate import router as auth_router
-from app.server.auth.authorize import TokenBearer
-
 from app.server.routes import router
 
+
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
+
 
 origins = ["*"]
 
